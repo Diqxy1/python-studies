fbc268cfb85e3af6414e2b7bf4494bfeab6c3d00
Diqxy1 <kusanagitakeru@outlook.com>
2026-02-25 22:07:27 -0300

    refactor: reorganize employee registration flow and improve data saving functionality

--- oop/Empresa/main.py
+++ oop/Empresa/main.py
@@ -1,20 +1,51 @@
+import json
 from funcionarios import Funcionarios 
-#CRUD Funcionarios
-# nome, setor, cargo, horario, salario
-def valida_int(mensagem):
-    valor = input(mensagem)
-    Try:
-      int(valor)
-    
 
-    
+def salvar_dados(lista, arquivo="empresa.json"):
+    # convertemos todos os objetos para dict usando a função to_dict da class funcionarios
+    dados_dict = [funcionario.to_dict() for funcionario in lista]
+    with open(arquivo, "w", encoding="utf-8") as f:
+        json.dump(dados_dict, f, indent=4, ensure_ascii=False)
+    print(f"\ndados salvos com sucesso em {arquivo}!")
+
+lista_funcionarios = []
+
 while True:
-    print("----- Ala Funcionarios-----")
-    print(""" 
-          1 - Adicionar Novo Funcionario \n
-          2 - Listar Funcionarios \n
-          3 - Excluir Funcionario \n
-          0 - Sair     
-    """)
-    escolha = input('escolha : ').lower
-    if escolha == 1
+    print("\n" + "="*30)
+    print("SISTEMA DE CADASTRO")
+    print("="*30)
+    try:
+      nome = input("nome: ")
+      setor = input("setor: ")
+      cargo = input("cargo: ")
+        
+      # erro de tipo costuma acontecer aqui ou no setter
+      salario_input = input("salario : ")
+      salario = float(salario_input) # se vier letra, o ValueError pula direto pro except
+
+      novo = Funcionarios(nome, setor, cargo, salario)
+
+      lista_funcionarios.append(novo)
+      print("\nfuncionario adicionado a lista")
+
+    except ValueError as e:
+        # aqui pegamos erros de conversão (letras no float/int) 
+        # e erros dos seus Setters (salário negativo, bônus > 100, etc)
+        print(f"\nERRO DE INPUT: {e}")
+        print("insira valores validos.")
+        continue # reinicia o loop para tentar novamente
+
+    except Exception as e:
+        # para qualquer outro erro inesperado
+        print(f"\nocorreu um erro inesperado: {e}")
+        continue
+
+    sair = input("\ndeseja cadastrar outro? (s/n): ").lower()
+    if sair == 'n':
+        break
+
+# no final do loop, salvamos tudo
+if lista_funcionarios:
+    salvar_dados(lista_funcionarios)
+else:
+    print("\nnenhum dado foi cadastrado para salvar.")
